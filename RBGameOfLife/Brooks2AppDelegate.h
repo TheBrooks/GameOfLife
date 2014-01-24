496029d706056f9fbdbb4a409aaeb18d3f09ce5f
Ryan Brooks <dootfiddle@Ryan-Brookss-MacBook-Pro.local>
2014-01-24 00:45:39 -0600

    initial commit

---
+++ RBGameOfLife/Brooks2AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Brooks2AppDelegate.h
+//  RBGameOfLife
+//
+//  Created by Ryan Brooks on 1/24/14.
+//  Copyright (c) 2014 Ryan Brooks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Brooks2AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RBGameOfLife/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RBGameOfLife
+//
+//  Created by Ryan Brooks on 1/24/14.
+//  Copyright (c) 2014 Ryan Brooks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Brooks2AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Brooks2AppDelegate class]));
+    }
+}
